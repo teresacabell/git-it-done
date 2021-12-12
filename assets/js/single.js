c5efd298a2506dfcfd1ef29bb248dc5dab8e3014
teresacabell <teresacabll@gmail.com>
2021-12-12 16:23:03 -0600

    added repo search

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,23 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var getRepoName = function() {
+// grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];  
+
+  // redirect if query parameter is unavailable
+  if(repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+    
+    getRepoIssues(repoName);
+  } else {
+    // if no repo given, redirect to homepage
+    document.location.replace("./index.html");
+  }
+};
+
 var getRepoIssues = function(repo) {
   // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -20,8 +37,7 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      console.log(response);
-      alert("There was a problem with your request!");
+      document.location.replace("./index.html")
     }
   });
 };
@@ -82,3 +98,4 @@ var displayWarning = function(repo) {
 };
 
 getRepoIssues("facebook/react");
+getRepoName();
